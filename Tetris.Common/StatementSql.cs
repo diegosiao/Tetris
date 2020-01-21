20705920275bd6089d4b01d136adc11623176abe
_grecio <grecio@gmail.com>
2020-01-20 22:17:18 -0300

    remove unused namespace

--- Tetris.Common/StatementSql.cs
+++ Tetris.Common/StatementSql.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tetris.Common
 {
